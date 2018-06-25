84e99ed86e6b4c2ffe1fc7f0c61f6e58687e22c7
Matthew Peveler <matt.peveler@gmail.com>
2018-06-25 18:10:53 -0400

    initial attempt at this

---
+++ build.sh
@@ -0,0 +1,16 @@
+## Docker configuration details
+USERNAME=masterodin
+IMAGE=python
+
+run_build=false
+docker pull masterodin/python:${TRAVIS_PYTHON_VERSION}
+if [ $? -eq 0 ]; then
+  # check python and pip version
+else
+  run_build=true
+fi
+
+if [ "${run_build}" = true ]; then
+  docker build -t ${USERNAME}/${IMAGE}:${TRAVIS_PYTHON_VERSION} --build-arg PYTHON_VERSION=${PYTHON_VERSION} --build-arg PYTHON_PIP_VERSION=${PYTHON_PIP_VERSION} .
+  docker push ${USERNAME}/${IMAGE}:${TRAVIS_PYTHON_VERSION}
+fi
