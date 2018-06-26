827e4113b6eda97d9e85aa132b30b55d16396339
Matthew Peveler <matt.peveler@gmail.com>
2018-06-26 17:33:40 -0400

    use proper image name for test

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ docker pull ${IMAGE_NAME}
 set -ex
 if [ $? -eq 0 ]; then
   # check python and pip version
-  PY_VERSION=$(docker run masterodin/python:3.6 python --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}")
+  PY_VERSION=$(docker run ${IMAGE_NAME} python --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}")
   PYPI_VERSION=$(docker run ${IMAGE_NAME} pip --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}" | head -1)
   echo "Docker:"
   echo "    Python: ${PY_VERSION}"
@@ -22,12 +22,7 @@ if [ $? -eq 0 ]; then
     echo "Docker and Local versions match. Doing nothing."
     exit 0
   fi
-else
-  run_build=true
 fi
 
-if [ "${run_build}" = true ]; then
-  set -ex
-  docker build -t ${IMAGE_NAME} --build-arg PYTHON_VERSION="${PYTHON_VERSION}" --build-arg PYTHON_PIP_VERSION="${PYTHON_PIP_VERSION}" .
-  docker push ${IMAGE_NAME}
-fi
+docker build -t ${IMAGE_NAME} --build-arg PYTHON_VERSION="${PYTHON_VERSION}" --build-arg PYTHON_PIP_VERSION="${PYTHON_PIP_VERSION}" .
+docker push ${IMAGE_NAME}
