4cb863fd12f1147262094142d8d0a4a7547c9a39
Matthew Peveler <matt.peveler@gmail.com>
2018-06-26 17:00:19 -0400

    improve build process

--- build.sh
+++ build.sh
@@ -2,17 +2,31 @@
 USERNAME=masterodin
 IMAGE=python
 
-run_build=false
-docker pull masterodin/python:${TRAVIS_PYTHON_VERSION}
+echo "Local:"
+echo "    Python: ${PYTHON_VERSION}"
+echo "    Pip   : ${PYPI_VERSION}"
+echo ""
+
+IMAGE_NAME="${USERNAME}/${IMAGE}:${TRAVIS_PYTHON_VERSION}"
+docker pull ${IMAGE_NAME}
 if [ $? -eq 0 ]; then
   # check python and pip version
-  echo "need to check"
+  PY_VERSION=$(docker run masterodin/python:3.6 python --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}")
+  PYPI_VERSION=$(dockerrun ${IMAGE_NAME} pip --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}" | head -1)
+  echo "Docker:"
+  echo "    Python: ${PY_VERSION}"
+  echo "    Pip   : ${PYPI_VERSION}"
+
+  if [ "${PY_VERSION}" = "${PYTHON_VERSION}"] && [ "${PYPI_VERSION}" = "${PYTHON_PIP_VERSION}" ]; then
+    echo "Docker and Local versions match. Doing nothing."
+    exit 0
+  fi
 else
   run_build=true
 fi
 
 if [ "${run_build}" = true ]; then
   set -ex
-  docker build -t ${USERNAME}/${IMAGE}:${TRAVIS_PYTHON_VERSION} --build-arg PYTHON_VERSION="${PYTHON_VERSION}" --build-arg PYTHON_PIP_VERSION="${PYTHON_PIP_VERSION}" .
-  docker push ${USERNAME}/${IMAGE}:${TRAVIS_PYTHON_VERSION}
+  docker build -t ${IMAGE_NAME} --build-arg PYTHON_VERSION="${PYTHON_VERSION}" --build-arg PYTHON_PIP_VERSION="${PYTHON_PIP_VERSION}" .
+  docker push ${IMAGE_NAME}
 fi
