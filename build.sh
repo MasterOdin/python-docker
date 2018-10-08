a601a39df35bd24f26cb0c35dda30abf9e1b9747
Matthew Peveler <matt.peveler@gmail.com>
2018-10-08 16:30:01 -0400

    Fix pip version not being detected if only have two version parts

--- build.sh
+++ build.sh
@@ -13,19 +13,26 @@ LAST=$?
 set -e
 if [ "${LAST}" -eq 0 ]; then
   # check python and pip version
-  PY_VERSION=$(docker run -t ${IMAGE_NAME} python --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}")
-  PYPI_VERSION=$(docker run -t ${IMAGE_NAME} pip --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}" | head -1)
+  PY_VERSION=$(docker run --rm ${IMAGE_NAME} python --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}")
+  PYPI_VERSION=$(docker run --rm ${IMAGE_NAME} pip --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}" | head -1)
+  if [ -z "${PYPI_VERSION}" ]; then
+    PYPI_VERSION=$(docker run --rm ${IMAGE_NAME} pip --version | grep -o "[0-9]\{1,2\}.[0-9]\{1,2\}" | head -1)
+  fi
   echo "Docker:"
   echo "    Python: ${PY_VERSION}"
   echo "    Pip   : ${PYPI_VERSION}"
+  echo ""
 
   if [ "${PY_VERSION}" = "${PYTHON_VERSION}" ] && [ "${PYPI_VERSION}" = "${PYTHON_PIP_VERSION}" ]; then
     echo "Docker and Local versions match. Doing nothing."
     exit 0
+  else
+    echo "Docker and Local versions mismatch. Building new image."
   fi
 else
   echo "No docker build found on Hub. Building fresh image."
 fi
+echo ""
 
 docker build -t ${IMAGE_NAME} --build-arg PYTHON_VERSION="${PYTHON_VERSION}" --build-arg PYTHON_PIP_VERSION="${PYTHON_PIP_VERSION}" .
 docker push ${IMAGE_NAME}
